e5d96c07983db979b5c67d83b68256b0f0977de2
岩本 <k_iwamoto@stride-web.co.jp>
2023-03-14 13:14:11 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
+import Head from "next/head";
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+			<Head>
+				<title>RapidAPI Devrel Example - Hotel App</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700;800&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<Component {...pageProps} />;
+		</>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,82 +4,53 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="flex flex-col md:px-12 px-0 relative bg-zinc-200 font-raleway items-center min-h-screen">
+			<h1 className="text-6xl text-primary font-bold mt-20">
+				Hotel Booking <span className="text-active">App</span>
+			</h1>
+			<h2 className="text-active text-2xl mt-6">
+				Discover and Book Hotels using Hotels API from RapidAPI Hub.
+			</h2>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="sm:mx-auto mt-20 justify-center sm:w-full sm:flex">
+        <input 
+          type="text"
+          className='block w-1/3 border border-transparent rounded-md px-5 py-3 text-base text-background shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-active'
+          placeholder='Enter your destination city'
+        />
+        <div className='mt-4 sm:mt-0 sm:ml-3'>
+          <button className='block w-full rounded-md px-5 py-3 bg-active text-base font-medium text-primary focus:outline-none focus:ring-2 focus:ring-primary sm:px-10'>
+            Search
+          </button>
         </div>
-      </main>
+      </div>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+      <div className='mt-10 w-full sm:mx-auto lg:mx-0'>
+        <div className='md:grid md:grid-cols-6 gap-1 flex flex-col'>
+          <div className='rounded-l-lg col-span-2 flex flex-col py-2 items-center bg-primary'>
+            <label htmlFor="check-in" className='py-2 text-sm font-semibold uppercase'>Check-in</label>
+            <input id='startDate' type="date" />
+          </div>
+
+          <div className='rounded-l-lg col-span-2 flex flex-col py-2 items-center bg-primary'>
+            <label htmlFor="check-out" className='py-2 text-sm font-semibold uppercase'>Check-out</label>
+            <input id='check-out' type="date" />
+          </div>
+
+          <div className="col-span-1 py-2 flex flex-col items-center bg-primary overflow-hidden">
+						<label htmlFor="guests" className="py-2 text-sm font-semibold uppercase">Guests</label>
+						<input id="guests" type="number" placeholder="Total guests" className="text-center" />
+					</div>
+
+					<div className="col-span-1 bg-active hover:opacity-80 rounded-r-lg">
+						<button type="submit" className="w-full h-full md:py-0 py-4 text-primary font-bold break-words">
+							Find Hotels
+						</button>
+					</div>
+        </div>
+      </div>
+
+		</div>
   )
 }
 
